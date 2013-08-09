5faed86418f15799cc8463ce9b9a3f248408e23f
Abe Voelker <abe@abevoelker.com>
2013-08-09 15:19:07 -0500

    Simplify :full formatter blank/nil spec using helpers

--- spec/formatters/full_spec.rb
+++ spec/formatters/full_spec.rb
@@ -3,6 +3,16 @@ require File.dirname(__FILE__) + '/formatters_helper'
 describe Nomen::Formatters::Full do
   context 'formatting a name' do
 
+    # transforms #<Nomen @first="Bob"> into #<Nomen @first="Bob", @middle=nil, @last=nil, suffix=nil>
+    def nils(nomen)
+      Nomen.new(nomen.to_h)
+    end
+
+    # transforms #<Nomen @first="Bob"> into #<Nomen @first="Bob", @middle="", @last="", suffix="">
+    def blanks(nomen)
+      Nomen.new(nomen.to_h.inject({}){|a,(k,v)| a[k] = (v.nil? ? '' : v); a })
+    end
+
     # name segment combinations were generated with
     # f = Nomen::FRAGMENTS; f.size.downto(1).map{|i| f.combination(i).to_a}.flatten(1)
 
@@ -12,203 +22,177 @@ describe Nomen::Formatters::Full do
       end
 
       it 'should format correctly' do
-        expect(Nomen::Formatters::Full.format(@name)).to eq('Katya Verenice Voelker M.D.')
+        expect(Nomen::Formatters::Full.format(@name)).to         eq('Katya Verenice Voelker M.D.')
+        expect(Nomen::Formatters::Full.format(nils(@name))).to   eq(Nomen::Formatters::Full.format(@name))
+        expect(Nomen::Formatters::Full.format(blanks(@name))).to eq(Nomen::Formatters::Full.format(@name))
       end
     end
 
     context 'with [:first, :middle, :last]' do
       before do
-        @name   = Nomen.new(first: 'Katya', middle: 'Verenice', last: 'Voelker')
-        @nils   = Nomen.new(first: 'Katya', middle: 'Verenice', last: 'Voelker', suffix: nil)
-        @blanks = Nomen.new(first: 'Katya', middle: 'Verenice', last: 'Voelker', suffix: '')
+        @name = Nomen.new(first: 'Katya', middle: 'Verenice', last: 'Voelker')
       end
 
       it 'should format correctly' do
-        expect(Nomen::Formatters::Full.format(@name)).to   eq('Katya Verenice Voelker')
-        expect(Nomen::Formatters::Full.format(@nils)).to   eq(Nomen::Formatters::Full.format(@name))
-        expect(Nomen::Formatters::Full.format(@blanks)).to eq(Nomen::Formatters::Full.format(@name))
+        expect(Nomen::Formatters::Full.format(@name)).to         eq('Katya Verenice Voelker')
+        expect(Nomen::Formatters::Full.format(nils(@name))).to   eq(Nomen::Formatters::Full.format(@name))
+        expect(Nomen::Formatters::Full.format(blanks(@name))).to eq(Nomen::Formatters::Full.format(@name))
       end
     end
 
     context 'with [:first, :middle, :suffix]' do
       before do
-        @name   = Nomen.new(first: 'Katya', middle: 'Verenice', suffix: 'M.D.')
-        @nils   = Nomen.new(first: 'Katya', middle: 'Verenice', suffix: 'M.D.', last: nil)
-        @blanks = Nomen.new(first: 'Katya', middle: 'Verenice', suffix: 'M.D.', last: '')
+        @name = Nomen.new(first: 'Katya', middle: 'Verenice', suffix: 'M.D.')
       end
 
       it 'should format correctly' do
-        expect(Nomen::Formatters::Full.format(@name)).to   eq('Katya Verenice M.D.')
-        expect(Nomen::Formatters::Full.format(@nils)).to   eq(Nomen::Formatters::Full.format(@name))
-        expect(Nomen::Formatters::Full.format(@blanks)).to eq(Nomen::Formatters::Full.format(@name))
+        expect(Nomen::Formatters::Full.format(@name)).to         eq('Katya Verenice M.D.')
+        expect(Nomen::Formatters::Full.format(nils(@name))).to   eq(Nomen::Formatters::Full.format(@name))
+        expect(Nomen::Formatters::Full.format(blanks(@name))).to eq(Nomen::Formatters::Full.format(@name))
       end
     end
 
     context 'with [:first, :last, :suffix]' do
       before do
-        @name   = Nomen.new(first: 'Katya', last: 'Voelker', suffix: 'M.D.')
-        @nils   = Nomen.new(first: 'Katya', last: 'Voelker', suffix: 'M.D.', middle: nil)
-        @blanks = Nomen.new(first: 'Katya', last: 'Voelker', suffix: 'M.D.', middle: '')
+        @name = Nomen.new(first: 'Katya', last: 'Voelker', suffix: 'M.D.')
       end
 
       it 'should format correctly' do
-        expect(Nomen::Formatters::Full.format(@name)).to   eq('Katya Voelker M.D.')
-        expect(Nomen::Formatters::Full.format(@nils)).to   eq(Nomen::Formatters::Full.format(@name))
-        expect(Nomen::Formatters::Full.format(@blanks)).to eq(Nomen::Formatters::Full.format(@name))
+        expect(Nomen::Formatters::Full.format(@name)).to         eq('Katya Voelker M.D.')
+        expect(Nomen::Formatters::Full.format(nils(@name))).to   eq(Nomen::Formatters::Full.format(@name))
+        expect(Nomen::Formatters::Full.format(blanks(@name))).to eq(Nomen::Formatters::Full.format(@name))
       end
     end
 
     context 'with [:middle, :last, :suffix]' do
       before do
-        @name   = Nomen.new(middle: 'Verenice', last: 'Voelker', suffix: 'M.D.')
-        @nils   = Nomen.new(middle: 'Verenice', last: 'Voelker', suffix: 'M.D.', first: nil)
-        @blanks = Nomen.new(middle: 'Verenice', last: 'Voelker', suffix: 'M.D.', first: '')
+        @name = Nomen.new(middle: 'Verenice', last: 'Voelker', suffix: 'M.D.')
       end
 
       it 'should format correctly' do
-        expect(Nomen::Formatters::Full.format(@name)).to   eq('Verenice Voelker M.D.')
-        expect(Nomen::Formatters::Full.format(@nils)).to   eq(Nomen::Formatters::Full.format(@name))
-        expect(Nomen::Formatters::Full.format(@blanks)).to eq(Nomen::Formatters::Full.format(@name))
+        expect(Nomen::Formatters::Full.format(@name)).to         eq('Verenice Voelker M.D.')
+        expect(Nomen::Formatters::Full.format(nils(@name))).to   eq(Nomen::Formatters::Full.format(@name))
+        expect(Nomen::Formatters::Full.format(blanks(@name))).to eq(Nomen::Formatters::Full.format(@name))
       end
     end
 
     context 'with [:first, :middle]' do
       before do
-        @name   = Nomen.new(first: 'Katya', middle: 'Verenice')
-        @nils   = Nomen.new(first: 'Katya', middle: 'Verenice', last: nil, suffix: nil)
-        @blanks = Nomen.new(first: 'Katya', middle: 'Verenice', last: '',  suffix: '')
+        @name = Nomen.new(first: 'Katya', middle: 'Verenice')
       end
 
       it 'should format correctly' do
-        expect(Nomen::Formatters::Full.format(@name)).to   eq('Katya Verenice')
-        expect(Nomen::Formatters::Full.format(@nils)).to   eq(Nomen::Formatters::Full.format(@name))
-        expect(Nomen::Formatters::Full.format(@blanks)).to eq(Nomen::Formatters::Full.format(@name))
+        expect(Nomen::Formatters::Full.format(@name)).to         eq('Katya Verenice')
+        expect(Nomen::Formatters::Full.format(nils(@name))).to   eq(Nomen::Formatters::Full.format(@name))
+        expect(Nomen::Formatters::Full.format(blanks(@name))).to eq(Nomen::Formatters::Full.format(@name))
       end
     end
 
     context 'with [:first, :last]' do
       before do
-        @name   = Nomen.new(first: 'Katya', last: 'Voelker')
-        @nils   = Nomen.new(first: 'Katya', last: 'Voelker', middle: nil, suffix: nil)
-        @blanks = Nomen.new(first: 'Katya', last: 'Voelker', middle: '',  suffix: '')
+        @name = Nomen.new(first: 'Katya', last: 'Voelker')
       end
 
       it 'should format correctly' do
-        expect(Nomen::Formatters::Full.format(@name)).to   eq('Katya Voelker')
-        expect(Nomen::Formatters::Full.format(@nils)).to   eq(Nomen::Formatters::Full.format(@name))
-        expect(Nomen::Formatters::Full.format(@blanks)).to eq(Nomen::Formatters::Full.format(@name))
+        expect(Nomen::Formatters::Full.format(@name)).to         eq('Katya Voelker')
+        expect(Nomen::Formatters::Full.format(nils(@name))).to   eq(Nomen::Formatters::Full.format(@name))
+        expect(Nomen::Formatters::Full.format(blanks(@name))).to eq(Nomen::Formatters::Full.format(@name))
       end
     end
 
     context 'with [:first, :suffix]' do
       before do
-        @name   = Nomen.new(first: 'Katya', suffix: 'M.D.')
-        @nils   = Nomen.new(first: 'Katya', suffix: 'M.D.', middle: nil, last: nil)
-        @blanks = Nomen.new(first: 'Katya', suffix: 'M.D.', middle: '',  last: '')
+        @name = Nomen.new(first: 'Katya', suffix: 'M.D.')
       end
 
       it 'should format correctly' do
-        expect(Nomen::Formatters::Full.format(@name)).to   eq('Katya M.D.')
-        expect(Nomen::Formatters::Full.format(@nils)).to   eq(Nomen::Formatters::Full.format(@name))
-        expect(Nomen::Formatters::Full.format(@blanks)).to eq(Nomen::Formatters::Full.format(@name))
+        expect(Nomen::Formatters::Full.format(@name)).to         eq('Katya M.D.')
+        expect(Nomen::Formatters::Full.format(nils(@name))).to   eq(Nomen::Formatters::Full.format(@name))
+        expect(Nomen::Formatters::Full.format(blanks(@name))).to eq(Nomen::Formatters::Full.format(@name))
       end
     end
 
     context 'with [:middle, :last]' do
       before do
-        @name   = Nomen.new(middle: 'Verenice', last: 'Voelker')
-        @nils   = Nomen.new(middle: 'Verenice', last: 'Voelker', first: nil, suffix: nil)
-        @blanks = Nomen.new(middle: 'Verenice', last: 'Voelker', first: '', suffix: '')
+        @name = Nomen.new(middle: 'Verenice', last: 'Voelker')
       end
 
       it 'should format correctly' do
-        expect(Nomen::Formatters::Full.format(@name)).to   eq('Verenice Voelker')
-        expect(Nomen::Formatters::Full.format(@nils)).to   eq(Nomen::Formatters::Full.format(@name))
-        expect(Nomen::Formatters::Full.format(@blanks)).to eq(Nomen::Formatters::Full.format(@name))
+        expect(Nomen::Formatters::Full.format(@name)).to         eq('Verenice Voelker')
+        expect(Nomen::Formatters::Full.format(nils(@name))).to   eq(Nomen::Formatters::Full.format(@name))
+        expect(Nomen::Formatters::Full.format(blanks(@name))).to eq(Nomen::Formatters::Full.format(@name))
       end
     end
 
     context 'with [:middle, :suffix]' do
       before do
-        @name   = Nomen.new(middle: 'Verenice', suffix: 'M.D.')
-        @nils   = Nomen.new(middle: 'Verenice', suffix: 'M.D.', first: nil, last: nil)
-        @blanks = Nomen.new(middle: 'Verenice', suffix: 'M.D.', first: '',  last: '')
+        @name = Nomen.new(middle: 'Verenice', suffix: 'M.D.')
       end
 
       it 'should format correctly' do
-        expect(Nomen::Formatters::Full.format(@name)).to   eq('Verenice M.D.')
-        expect(Nomen::Formatters::Full.format(@nils)).to   eq(Nomen::Formatters::Full.format(@name))
-        expect(Nomen::Formatters::Full.format(@blanks)).to eq(Nomen::Formatters::Full.format(@name))
+        expect(Nomen::Formatters::Full.format(@name)).to         eq('Verenice M.D.')
+        expect(Nomen::Formatters::Full.format(nils(@name))).to   eq(Nomen::Formatters::Full.format(@name))
+        expect(Nomen::Formatters::Full.format(blanks(@name))).to eq(Nomen::Formatters::Full.format(@name))
       end
     end
 
     context 'with [:last, :suffix]' do
       before do
-        @name   = Nomen.new(last: 'Voelker', suffix: 'M.D.')
-        @nils   = Nomen.new(last: 'Voelker', suffix: 'M.D.', first: nil, middle: nil)
-        @blanks = Nomen.new(last: 'Voelker', suffix: 'M.D.', first: '',  middle: '')
+        @name = Nomen.new(last: 'Voelker', suffix: 'M.D.')
       end
 
       it 'should format correctly' do
-        expect(Nomen::Formatters::Full.format(@name)).to   eq('Voelker M.D.')
-        expect(Nomen::Formatters::Full.format(@nils)).to   eq(Nomen::Formatters::Full.format(@name))
-        expect(Nomen::Formatters::Full.format(@blanks)).to eq(Nomen::Formatters::Full.format(@name))
+        expect(Nomen::Formatters::Full.format(@name)).to         eq('Voelker M.D.')
+        expect(Nomen::Formatters::Full.format(nils(@name))).to   eq(Nomen::Formatters::Full.format(@name))
+        expect(Nomen::Formatters::Full.format(blanks(@name))).to eq(Nomen::Formatters::Full.format(@name))
       end
     end
 
     context 'with [:first]' do
       before do
-        @name   = Nomen.new(first: 'Katya')
-        @nils   = Nomen.new(first: 'Katya', middle: nil, last: nil, suffix: nil)
-        @blanks = Nomen.new(first: 'Katya', middle: '',  last: '',  suffix: '')
+        @name = Nomen.new(first: 'Katya')
       end
 
       it 'should format correctly' do
-        expect(Nomen::Formatters::Full.format(@name)).to   eq('Katya')
-        expect(Nomen::Formatters::Full.format(@nils)).to   eq(Nomen::Formatters::Full.format(@name))
-        expect(Nomen::Formatters::Full.format(@blanks)).to eq(Nomen::Formatters::Full.format(@name))
+        expect(Nomen::Formatters::Full.format(@name)).to         eq('Katya')
+        expect(Nomen::Formatters::Full.format(nils(@name))).to   eq(Nomen::Formatters::Full.format(@name))
+        expect(Nomen::Formatters::Full.format(blanks(@name))).to eq(Nomen::Formatters::Full.format(@name))
       end
     end
 
     context 'with [:middle]' do
       before do
-        @name   = Nomen.new(middle: 'Verenice')
-        @nils   = Nomen.new(middle: 'Verenice', first: nil, last: nil, suffix: nil)
-        @blanks = Nomen.new(middle: 'Verenice', first: '',  last: '',  suffix: '')
+        @name = Nomen.new(middle: 'Verenice')
       end
 
       it 'should format correctly' do
-        expect(Nomen::Formatters::Full.format(@name)).to   eq('Verenice')
-        expect(Nomen::Formatters::Full.format(@nils)).to   eq(Nomen::Formatters::Full.format(@name))
-        expect(Nomen::Formatters::Full.format(@blanks)).to eq(Nomen::Formatters::Full.format(@name))
+        expect(Nomen::Formatters::Full.format(@name)).to         eq('Verenice')
+        expect(Nomen::Formatters::Full.format(nils(@name))).to   eq(Nomen::Formatters::Full.format(@name))
+        expect(Nomen::Formatters::Full.format(blanks(@name))).to eq(Nomen::Formatters::Full.format(@name))
       end
     end
 
     context 'with [:last]' do
       before do
-        @name   = Nomen.new(last: 'Voelker')
-        @nils   = Nomen.new(last: 'Voelker', middle: nil, first: nil, suffix: nil)
-        @blanks = Nomen.new(last: 'Voelker', middle: '', first: '',  suffix: '')
+        @name = Nomen.new(last: 'Voelker')
       end
 
       it 'should format correctly' do
-        expect(Nomen::Formatters::Full.format(@name)).to   eq('Voelker')
-        expect(Nomen::Formatters::Full.format(@nils)).to   eq(Nomen::Formatters::Full.format(@name))
-        expect(Nomen::Formatters::Full.format(@blanks)).to eq(Nomen::Formatters::Full.format(@name))
+        expect(Nomen::Formatters::Full.format(@name)).to         eq('Voelker')
+        expect(Nomen::Formatters::Full.format(nils(@name))).to   eq(Nomen::Formatters::Full.format(@name))
+        expect(Nomen::Formatters::Full.format(blanks(@name))).to eq(Nomen::Formatters::Full.format(@name))
       end
     end
 
     context 'with [:suffix]' do
       before do
-        @name   = Nomen.new(suffix: 'M.D.')
-        @nils   = Nomen.new(suffix: 'M.D.', middle: nil, first: nil, last: nil)
-        @blanks = Nomen.new(suffix: 'M.D.', middle: '',  first: '',  last: '')
+        @name = Nomen.new(suffix: 'M.D.')
       end
 
       it 'should format correctly' do
-        expect(Nomen::Formatters::Full.format(@name)).to   eq('M.D.')
-        expect(Nomen::Formatters::Full.format(@nils)).to   eq(Nomen::Formatters::Full.format(@name))
-        expect(Nomen::Formatters::Full.format(@blanks)).to eq(Nomen::Formatters::Full.format(@name))
+        expect(Nomen::Formatters::Full.format(@name)).to         eq('M.D.')
+        expect(Nomen::Formatters::Full.format(nils(@name))).to   eq(Nomen::Formatters::Full.format(@name))
+        expect(Nomen::Formatters::Full.format(blanks(@name))).to eq(Nomen::Formatters::Full.format(@name))
       end
     end
   end
