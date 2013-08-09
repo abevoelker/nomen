463c70636cc2b1d07b713b6bbd7bd54d3d33bcb9
Abe Voelker <abe@abevoelker.com>
2013-08-09 15:24:49 -0500

    Correct :inverted formatter handling of nil/empty string

--- lib/nomen/formatters/inverted.rb
+++ lib/nomen/formatters/inverted.rb
@@ -4,20 +4,20 @@ class Nomen
 
       def self.format(name)
         String.new.tap do |str|
-          if name.last
+          if name.last && !name.last.empty?
             if [name.first, name.middle, name.suffix].reject(&:nil?).reject(&:empty?).any?
               str << "#{name.last},"
             else
               str << name.last
             end
           end
-          if name.first
+          if name.first && !name.first.empty?
             str << (str.empty? ? name.first : " #{name.first}")
           end
-          if name.middle
+          if name.middle && !name.middle.empty?
             str << (str.empty? ? name.middle : " #{name.middle}")
           end
-          if name.suffix
+          if name.suffix && !name.suffix.empty?
             str << (str.empty? ? name.suffix : " #{name.suffix}")
           end
         end
--- spec/formatters/inverted_spec.rb
+++ spec/formatters/inverted_spec.rb
@@ -3,6 +3,16 @@ require File.dirname(__FILE__) + '/formatters_helper'
 describe Nomen::Formatters::Inverted do
   context 'formatting a name' do
 
+    # transforms #<Nomen @first="Bob"> into #<Nomen @first="Bob", @middle=nil, @last=nil, suffix=nil>
+    def nils(nomen)
+      Nomen.new(nomen.to_h)
+    end
+
+    # transforms #<Nomen @first="Bob"> into #<Nomen @first="Bob", @middle="", @last="", suffix="">
+    def blanks(nomen)
+      Nomen.new(nomen.to_h.inject({}){|a,(k,v)| a[k] = (v.nil? ? '' : v); a })
+    end
+
     # name segment combinations were generated with
     # f = Nomen::FRAGMENTS; f.size.downto(1).map{|i| f.combination(i).to_a}.flatten(1)
 
@@ -12,7 +22,9 @@ describe Nomen::Formatters::Inverted do
       end
 
       it 'should format correctly' do
-        expect(Nomen::Formatters::Inverted.format(@name)).to eq('Voelker, Katya Verenice M.D.')
+        expect(Nomen::Formatters::Inverted.format(@name)).to         eq('Voelker, Katya Verenice M.D.')
+        expect(Nomen::Formatters::Inverted.format(nils(@name))).to   eq(Nomen::Formatters::Inverted.format(@name))
+        expect(Nomen::Formatters::Inverted.format(blanks(@name))).to eq(Nomen::Formatters::Inverted.format(@name))
       end
     end
 
@@ -22,7 +34,9 @@ describe Nomen::Formatters::Inverted do
       end
 
       it 'should format correctly' do
-        expect(Nomen::Formatters::Inverted.format(@name)).to eq('Voelker, Katya Verenice')
+        expect(Nomen::Formatters::Inverted.format(@name)).to         eq('Voelker, Katya Verenice')
+        expect(Nomen::Formatters::Inverted.format(nils(@name))).to   eq(Nomen::Formatters::Inverted.format(@name))
+        expect(Nomen::Formatters::Inverted.format(blanks(@name))).to eq(Nomen::Formatters::Inverted.format(@name))
       end
     end
 
@@ -32,7 +46,9 @@ describe Nomen::Formatters::Inverted do
       end
 
       it 'should format correctly' do
-        expect(Nomen::Formatters::Inverted.format(@name)).to eq('Katya Verenice M.D.')
+        expect(Nomen::Formatters::Inverted.format(@name)).to         eq('Katya Verenice M.D.')
+        expect(Nomen::Formatters::Inverted.format(nils(@name))).to   eq(Nomen::Formatters::Inverted.format(@name))
+        expect(Nomen::Formatters::Inverted.format(blanks(@name))).to eq(Nomen::Formatters::Inverted.format(@name))
       end
     end
 
@@ -42,7 +58,9 @@ describe Nomen::Formatters::Inverted do
       end
 
       it 'should format correctly' do
-        expect(Nomen::Formatters::Inverted.format(@name)).to eq('Voelker, Katya M.D.')
+        expect(Nomen::Formatters::Inverted.format(@name)).to         eq('Voelker, Katya M.D.')
+        expect(Nomen::Formatters::Inverted.format(nils(@name))).to   eq(Nomen::Formatters::Inverted.format(@name))
+        expect(Nomen::Formatters::Inverted.format(blanks(@name))).to eq(Nomen::Formatters::Inverted.format(@name))
       end
     end
 
@@ -52,7 +70,9 @@ describe Nomen::Formatters::Inverted do
       end
 
       it 'should format correctly' do
-        expect(Nomen::Formatters::Inverted.format(@name)).to eq('Voelker, Verenice M.D.')
+        expect(Nomen::Formatters::Inverted.format(@name)).to         eq('Voelker, Verenice M.D.')
+        expect(Nomen::Formatters::Inverted.format(nils(@name))).to   eq(Nomen::Formatters::Inverted.format(@name))
+        expect(Nomen::Formatters::Inverted.format(blanks(@name))).to eq(Nomen::Formatters::Inverted.format(@name))
       end
     end
 
@@ -62,7 +82,9 @@ describe Nomen::Formatters::Inverted do
       end
 
       it 'should format correctly' do
-        expect(Nomen::Formatters::Inverted.format(@name)).to eq('Katya Verenice')
+        expect(Nomen::Formatters::Inverted.format(@name)).to         eq('Katya Verenice')
+        expect(Nomen::Formatters::Inverted.format(nils(@name))).to   eq(Nomen::Formatters::Inverted.format(@name))
+        expect(Nomen::Formatters::Inverted.format(blanks(@name))).to eq(Nomen::Formatters::Inverted.format(@name))
       end
     end
 
@@ -72,7 +94,9 @@ describe Nomen::Formatters::Inverted do
       end
 
       it 'should format correctly' do
-        expect(Nomen::Formatters::Inverted.format(@name)).to eq('Voelker, Katya')
+        expect(Nomen::Formatters::Inverted.format(@name)).to         eq('Voelker, Katya')
+        expect(Nomen::Formatters::Inverted.format(nils(@name))).to   eq(Nomen::Formatters::Inverted.format(@name))
+        expect(Nomen::Formatters::Inverted.format(blanks(@name))).to eq(Nomen::Formatters::Inverted.format(@name))
       end
     end
 
@@ -82,7 +106,9 @@ describe Nomen::Formatters::Inverted do
       end
 
       it 'should format correctly' do
-        expect(Nomen::Formatters::Inverted.format(@name)).to eq('Katya M.D.')
+        expect(Nomen::Formatters::Inverted.format(@name)).to         eq('Katya M.D.')
+        expect(Nomen::Formatters::Inverted.format(nils(@name))).to   eq(Nomen::Formatters::Inverted.format(@name))
+        expect(Nomen::Formatters::Inverted.format(blanks(@name))).to eq(Nomen::Formatters::Inverted.format(@name))
       end
     end
 
@@ -92,7 +118,9 @@ describe Nomen::Formatters::Inverted do
       end
 
       it 'should format correctly' do
-        expect(Nomen::Formatters::Inverted.format(@name)).to eq('Voelker, Verenice')
+        expect(Nomen::Formatters::Inverted.format(@name)).to         eq('Voelker, Verenice')
+        expect(Nomen::Formatters::Inverted.format(nils(@name))).to   eq(Nomen::Formatters::Inverted.format(@name))
+        expect(Nomen::Formatters::Inverted.format(blanks(@name))).to eq(Nomen::Formatters::Inverted.format(@name))
       end
     end
 
@@ -102,7 +130,9 @@ describe Nomen::Formatters::Inverted do
       end
 
       it 'should format correctly' do
-        expect(Nomen::Formatters::Inverted.format(@name)).to eq('Verenice M.D.')
+        expect(Nomen::Formatters::Inverted.format(@name)).to         eq('Verenice M.D.')
+        expect(Nomen::Formatters::Inverted.format(nils(@name))).to   eq(Nomen::Formatters::Inverted.format(@name))
+        expect(Nomen::Formatters::Inverted.format(blanks(@name))).to eq(Nomen::Formatters::Inverted.format(@name))
       end
     end
 
@@ -112,7 +142,9 @@ describe Nomen::Formatters::Inverted do
       end
 
       it 'should format correctly' do
-        expect(Nomen::Formatters::Inverted.format(@name)).to eq('Voelker, M.D.')
+        expect(Nomen::Formatters::Inverted.format(@name)).to         eq('Voelker, M.D.')
+        expect(Nomen::Formatters::Inverted.format(nils(@name))).to   eq(Nomen::Formatters::Inverted.format(@name))
+        expect(Nomen::Formatters::Inverted.format(blanks(@name))).to eq(Nomen::Formatters::Inverted.format(@name))
       end
     end
 
@@ -122,7 +154,9 @@ describe Nomen::Formatters::Inverted do
       end
 
       it 'should format correctly' do
-        expect(Nomen::Formatters::Inverted.format(@name)).to eq('Katya')
+        expect(Nomen::Formatters::Inverted.format(@name)).to         eq('Katya')
+        expect(Nomen::Formatters::Inverted.format(nils(@name))).to   eq(Nomen::Formatters::Inverted.format(@name))
+        expect(Nomen::Formatters::Inverted.format(blanks(@name))).to eq(Nomen::Formatters::Inverted.format(@name))
       end
     end
 
@@ -132,7 +166,9 @@ describe Nomen::Formatters::Inverted do
       end
 
       it 'should format correctly' do
-        expect(Nomen::Formatters::Inverted.format(@name)).to eq('Verenice')
+        expect(Nomen::Formatters::Inverted.format(@name)).to         eq('Verenice')
+        expect(Nomen::Formatters::Inverted.format(nils(@name))).to   eq(Nomen::Formatters::Inverted.format(@name))
+        expect(Nomen::Formatters::Inverted.format(blanks(@name))).to eq(Nomen::Formatters::Inverted.format(@name))
       end
     end
 
@@ -142,7 +178,9 @@ describe Nomen::Formatters::Inverted do
       end
 
       it 'should format correctly' do
-        expect(Nomen::Formatters::Inverted.format(@name)).to eq('Voelker')
+        expect(Nomen::Formatters::Inverted.format(@name)).to         eq('Voelker')
+        expect(Nomen::Formatters::Inverted.format(nils(@name))).to   eq(Nomen::Formatters::Inverted.format(@name))
+        expect(Nomen::Formatters::Inverted.format(blanks(@name))).to eq(Nomen::Formatters::Inverted.format(@name))
       end
     end
 
@@ -152,7 +190,9 @@ describe Nomen::Formatters::Inverted do
       end
 
       it 'should format correctly' do
-        expect(Nomen::Formatters::Inverted.format(@name)).to eq('M.D.')
+        expect(Nomen::Formatters::Inverted.format(@name)).to         eq('M.D.')
+        expect(Nomen::Formatters::Inverted.format(nils(@name))).to   eq(Nomen::Formatters::Inverted.format(@name))
+        expect(Nomen::Formatters::Inverted.format(blanks(@name))).to eq(Nomen::Formatters::Inverted.format(@name))
       end
     end
   end
