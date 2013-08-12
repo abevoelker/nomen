9efcbf291775a6858110c93b7db290bf5de6eade
Abe Voelker <abe@abevoelker.com>
2013-08-12 10:39:20 -0500

    Shorten :full formatter

--- lib/nomen/formatters/full.rb
+++ lib/nomen/formatters/full.rb
@@ -3,20 +3,8 @@ class Nomen
     class Full
 
       def self.format(name)
-        String.new.tap do |str|
-          if name.first && !name.first.empty?
-            str << name.first
-          end
-          if name.middle && !name.middle.empty?
-            str << (str.empty? ? name.middle : " #{name.middle}")
-          end
-          if name.last && !name.last.empty?
-            str << (str.empty? ? name.last : " #{name.last}")
-          end
-          if name.suffix && !name.suffix.empty?
-            str << (str.empty? ? name.suffix : " #{name.suffix}")
-          end
-        end
+        frags = [:first, :middle, :last, :suffix].map{|f| name.send(f)}
+        frags.reject(&:nil?).reject(&:empty?).join(' ')
       end
 
     end
